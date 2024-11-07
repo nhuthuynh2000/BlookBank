9dee84ad8d02db5c08c0e829b62235c972d87842
nhuthuynh2000 <165478116+nhuthuynh2000@users.noreply.github.com>
2024-11-08 05:54:27 +0700

    Update

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace BloodBankManagement
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new DonorView());
+            Application.Run(new MainForm());
         }
     }
 }
